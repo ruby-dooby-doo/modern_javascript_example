beab5294350bc6cb06f3ce793de2c6b6c2d6da62
Brian <riceb53@gmail.com>
2018-08-16 20:13:03 -0500

    add webpack, webpack-cli and mathjs

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-console.log('im in javascript')
+var moment = require('moment');
 
-
+console.log('I am not in javascript');
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+import * as math from 'mathjs'
+
+// use math.js
+console.log(math.sqrt(-4)); // 2i
